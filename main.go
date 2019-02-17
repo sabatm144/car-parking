107c6241e045341f36260e992ae7ad2ebdf4756d
Mona <you@example.com>
2019-02-17 11:07:38 +0530

    create, park operations added

--- main.go
+++ main.go
@@ -1,52 +1,52 @@
 package main
 
 import (
-    "bufio"
-    "flag"
-    "fmt"
-    "os"
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"parking-lot/internals"
 )
 
 func main() {
 
-    mode := flag.Int("mode", 1, "Select a mode to run")
-    filePath := flag.String("filePath", "", "Say your input file path, rest i will do for you")
+	mode := flag.Int("mode", 1, "Select a mode to run")
+	filePath := flag.String("filePath", "", "Input file path please!")
 
-    flag.Parse()
+	flag.Parse()
 
-    // get the actual option values.
-    fmt.Println("mode:", *mode)
-    fmt.Println("filePath:", *filePath)
+	// get the actual option values.
+	fmt.Println("mode:", *mode)
+	fmt.Println("filePath:", *filePath)
 
-    if *mode == 2 {
-        fmt.Println("Processing file, please wait")
-        if *filePath == "" {
-            fmt.Println("Hey you didn’t mention your input file path")
-            flag.PrintDefaults()
-            os.Exit(1)
-        }
+	if *mode == 2 {
+		fmt.Println("Processing file, please wait")
+		if *filePath == "" {
+			fmt.Println("Hey you didn’t mention your input file path")
+			flag.PrintDefaults()
+			os.Exit(1)
+		}
 
-        //TODO: read a file path and execute the commands
+		internals.ReadFromFile(*filePath)
+		os.Exit(1)
+	}
 
-        os.Exit(1)
-    }
+	fmt.Println("Interactive mode: please type your operations")
+	scanner := bufio.NewScanner(os.Stdin)
 
-    fmt.Println("hey you look good, say what I want to do for you")
-    scanner := bufio.NewScanner(os.Stdin)
-    
-    for scanner.Scan() {
-        text := scanner.Text()
+	for scanner.Scan() {
+		text := scanner.Text()
 
-        //TODO: execute the command
+		//TODO: execute the command
 
-        if text == "exit" {
-            fmt.Println("Good bye")
-            os.Exit(1)
-        }
-    }
-    
-    if err := scanner.Err(); err != nil {
-        fmt.Fprintln(os.Stderr, "reading standard input:", err)
-    }
+		if text == "exit" {
+			fmt.Println("Good bye")
+			os.Exit(1)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
 
 }
